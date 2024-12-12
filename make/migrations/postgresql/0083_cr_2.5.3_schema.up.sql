b4a2e0c1cc5e3b6219452944735a73f73890e9e7
zhoujiawei <zhoujiawei.1@bytedance.com>
2024-12-12 15:22:17 +0800

    fix: change access_secret db length

--- make/migrations/postgresql/0083_cr_2.5.3_schema.up.sql
+++ make/migrations/postgresql/0083_cr_2.5.3_schema.up.sql
@@ -5,4 +5,5 @@ BEGIN;
    ALTER TABLE registry ADD column if not exists https_proxy text;
    ALTER TABLE registry ADD column if not exists no_proxy text;
    ALTER TABLE registry ADD column if not exists vpc_id text;
+   ALTER TABLE registry ALTER column access_secret TYPE text;
 END;
