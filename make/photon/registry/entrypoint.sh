01113530c9388fa6bfed4f56caf7b05431251a75
zhoujiawei <zhoujiawei.1@bytedance.com>
2025-11-06 10:45:59 +0800

    feat: registry set GOMEMLIMIT

--- make/photon/registry/entrypoint.sh
+++ make/photon/registry/entrypoint.sh
@@ -10,4 +10,18 @@ set -e
 
 /home/harbor/install_cert.sh
 
+MEM_LIMIT=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
+
+if [ "$MEM_LIMIT" -eq 9223372036854771712 ]; then
+    # 读取主机总内存（字节数）
+    HOST_MEM=$(grep MemTotal /proc/meminfo | awk '{print $2 * 1024}')  # MemTotal 单位是 KB，转换为字节
+    GOMEMLIMIT=$((HOST_MEM * 9 / 10))
+    echo "未设置容器内存限制，使用主机内存的 90%: $GOMEMLIMIT 字节"
+else
+    GOMEMLIMIT=$((MEM_LIMIT * 9 / 10))
+    echo "已设置容器内存限制，使用容器内存的 90%: $GOMEMLIMIT 字节"
+fi
+
+export GOMEMLIMIT=$GOMEMLIMIT
+
 exec /usr/bin/registry_DO_NOT_USE_GC serve /etc/registry/config.yml
